82ce212fc16d61b15de9fd30316a87d5e903d20a
Aicachi <N00455405@upn.pe>
2025-05-31 01:17:07 -0500

    Update Program.cs

--- Ejercicio1/Ejercicio1/Program.cs
+++ Ejercicio1/Ejercicio1/Program.cs
@@ -10,25 +10,7 @@ namespace Ejercicio1
     {
         static void Main(string[] args)
         {
-            int horTraba;
-            double costHora, sueldo;
-
-            Console.WriteLine("Ingrese las horas de trabajo: ");
-            horTraba = int.Parse(Console.ReadLine());
-            Console.WriteLine("Ingrese el costo por hora: ");
-            costHora = double.Parse(Console.ReadLine());
             
-            //proceso
-            sueldo = horTraba * costHora;
-
-            Console.WriteLine("El sueldo total es: " + sueldo);
-            Console.ReadKey();
-            Console.WriteLine("El sueldo total es: " + sueldo);
-            Console.ReadKey();
-            Console.WriteLine("El sueldo total es: " + sueldo);
-            Console.ReadKey();
-            Console.WriteLine("El sueldo total es: " + sueldo);
-            Console.ReadKey();
         }
     }
 }
